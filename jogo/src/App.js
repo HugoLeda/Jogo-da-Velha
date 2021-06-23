a050e53ef308d228642a9796804cae24cd37866e
João Hugo Leda <joaohugoleda@gmail.com>
2021-06-23 19:29:36 -0300

    SASS

--- jogo/src/App.js
+++ jogo/src/App.js
@@ -1,4 +1,24 @@
+import {useState} from 'react'
+
 function App() {
+
+  const jogoInicial = [['', '', ''], ['', '', ''], ['', '', '']]
+  const [jogo, setJogo] = useState(jogoInicial)
+  const [simboloAtual, setSimboloAtual] = useState('X')
+  const [jogando, setJogando] = useState(true)
+
+  const tabuleiro = (j) => {
+    return (
+      <div className="tabu">
+        <div className="tabuLinha">
+          <div className="casa" data-pos='00' onClick="">{j[0][0]}</div>
+          <div className="casa" data-pos='01' onClick="">{j[0][1]}</div>
+          <div className="casa" data-pos='02' onClick="">{j[0][2]}</div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       Olá
